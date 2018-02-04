4524d1ab40b782797c1371736bf6e07edf26dfdd
jenniferyien <jenniferllwu@gmail.com>
2018-02-03 21:53:36 -0500

    setup react app

---
+++ src/App.js
@@ -0,0 +1,70 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Checkbox from './Checkbox';
+
+const items = [
+  '3x4',
+  '5x2',
+  '4x4',
+  '4x5'
+];
+
+class App extends Component {
+  componentWillMount = () => {
+    this.selectedCheckboxes = new Set();
+  }
+
+  toggleCheckbox = label => {
+    if (this.selectedCheckboxes.has(label)) {
+      this.selectedCheckboxes.delete(label);
+    } else {
+      this.selectedCheckboxes.add(label);
+    }
+  }
+
+  handleFormSubmit = formSubmitEvent => {
+    formSubmitEvent.preventDefault();
+
+    for (const checkbox of this.selectedCheckboxes) {
+      console.log(checkbox, 'is selected.');
+    }
+  }
+
+  createCheckbox = label => (
+    <Checkbox
+            label={label}
+            handleCheckboxChange={this.toggleCheckbox}
+            key={label}
+        />
+  )
+
+  createCheckboxes = () => (
+    items.map(this.createCheckbox)
+  )
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to the Memory Game</h1>
+        </header>
+        <div className="container">
+        <div className="row">
+          <div className="col-sm-12">
+
+            <form onSubmit={this.handleFormSubmit}>
+              {this.createCheckboxes()}
+
+              <button className="btn btn-default" type="submit">Save</button>
+            </form>
+
+          </div>
+        </div>
+      </div>
+      </div>
+    );
+  }
+}
+
+export default App;
